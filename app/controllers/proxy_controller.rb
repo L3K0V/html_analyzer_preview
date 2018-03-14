27ce96e3c716f69213cb837c99ce2743091bbb64
Asen Lekov <asenlekoff@gmail.com>
2018-03-14 20:24:18 +0200

    Honor Cache-Control
    When requesting an URL, honor the cache control max-age if available and
    set this to expire time of the redis cache.

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -9,12 +9,37 @@ class ProxyController < ApplicationController
     modified = $redis.get(url)
 
     if modified.nil? || lifetime
+
+      maxage = get_maxage(url)
       modified = HtmlAnalyzer.modify(url)
       $redis.set(url, modified)
-      $redis.expire(url, lifetime || 7.days)
+      $redis.expire(url, lifetime || maxage)
     end
 
     render html: modified.html_safe
+  end
+
+  private
+
+  def get_maxage url
+    require 'net/http'
+    require 'uri'
+
+    uri = URI(url)
+    http = Net::HTTP.start(uri.host)
+    maxage = 7.days
+
+    resp = http.head(uri.path)
+    if resp['cache-control']
+      puts resp['cache-control']
+      resp['cache-control'].split.each do |elem|
+        maxage = elem.scan(/\d+/).first if elem.include? 'max-age='
+      end
+    end
+
+    http.finish
+
+    maxage
   end
 
   def proxy_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
   post 'proxy/modify'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
